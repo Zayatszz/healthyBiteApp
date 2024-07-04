26c0d3eab8dd50c03c54cb8aee72f857da25a618
Zayatszz <zayaz3802@gmail.com>
2024-07-04 11:32:49 +0800

    carwash Item haana nemegdhim bol

---
+++ components/CarWashItem.js
@@ -0,0 +1,100 @@
+import React from "react";
+import { View, Text, StyleSheet, TouchableOpacity, Image } from "react-native";
+const carwashImg = require("../assets/carwashApp1.png")
+const CarWashItem =({ carwash })=>{
+
+  
+    const images = {
+        'carwashApp1.png': require("../assets/carwashApp1.png"),
+        'carwashApp2.jpg': require("../assets/carwashApp2.jpg"),
+        'carwashApp3.jpg': require("../assets/carwashApp3.jpg"),
+        
+      };
+    return (
+        // <View style={styles.item}>
+        //     <Image style={styles.carwashImg} source={carwashImg}/>
+        //     <View style={styles.flex}>
+        //         <Text style={styles.paragraph}>{carwash.name}</Text>
+        //         <Text style={styles.paragraph}>20к-50к</Text>
+
+        //     </View>
+        //     <Text style={styles.paragraph}>УБ, Баянзүрх, 5-р хороо</Text>
+        // </View>
+        <View style={styles.item}>
+            
+            {/* <Image style={styles.carwashImg} source={"../assets/carwashApp1.png"} /> */}
+            <Image style={styles.carwashImg} source={images[carwash.imageUrl]} />
+      <View style={styles.flex}>
+        
+        <Text style={styles.paragraph}>{carwash.name} {carwash.imageUrl}</Text>
+        {/* <Text style={styles.paragraph}>{carwash.price}</Text> */}
+      </View>
+      <Text style={styles.paragraph}>{carwash.location}</Text>
+    </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    item:{
+        backgroundColor:"#FFF",
+        paddingBottom: 15,
+        width:280,
+        borderRadius: 10,
+        // flexDirection:'row',
+        // alignItems:'center',
+        // justifyContent:'space-between',
+        marginBottom:20,
+        marginRight:20
+    },
+    carwashImg:{
+        width:280,
+        height:200,
+        borderTopLeftRadius:10,
+        borderTopRightRadius:10,
+    },
+    flex:{
+        // padding:10,
+        // paddingLeft:10,
+        // paddingHorizontal:20,
+         width:'100%',
+         flexDirection:'row',
+         justifyContent:'space-between',
+         alignItems:'center',
+         
+       },
+       paragraph:{
+        paddingTop:10,
+        paddingHorizontal:10,
+        // fontSize:20,
+        fontWeight:'bold',
+        // textAlign: 'center', 
+        // color:'#FFF'
+      },
+    itemLeft:{
+        flexDirection:'row',
+        alignItems:'center',
+        flexWrap:'wrap',
+    },
+    square:{
+        width: 24,
+        height: 24,
+        backgroundColor: "#55BCF6",
+        opacity: 0.4,
+        borderRadius: 5,
+        marginRight:15,
+    },
+    itemText:{
+        maxWidth:'80%',
+    },
+    circular:{
+        width:12,
+        height:12,
+        borderColor:'#55BCF6',
+        borderWidth:2,
+        borderRadius:5,
+    },
+
+    
+});
+
+export default CarWashItem;
