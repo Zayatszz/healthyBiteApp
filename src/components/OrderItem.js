734a649c3f1884f2fd086efb9b6000933ae25d3f
Zayazts <tszayaz1728@gmail.com>
2024-08-30 10:59:02 +0800

    orderItem isClicked dropdown detail infp

--- src/components/OrderItem.js
+++ src/components/OrderItem.js
@@ -1,11 +1,12 @@
 // components/OrderItem.js
 
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import { format } from 'date-fns';
 import { utcToZonedTime } from 'date-fns-tz';
 import Feather from 'react-native-vector-icons/Feather';
 import Animated, { FadeInDown } from "react-native-reanimated";
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
 const formatTime = (datetime) => {
   const date = new Date(datetime);
@@ -13,6 +14,7 @@ const formatTime = (datetime) => {
 };
 
 const OrderItem = ({ order, index }) => {
+  const [isClicked, setIsClicked] = useState(false);
   return (
 <Animated.View entering={FadeInDown.delay(200*index)}>
 
@@ -26,22 +28,56 @@ const OrderItem = ({ order, index }) => {
       <Text style={styles.orderText}>Үнэ: {order.price}₮</Text> */}
 
       {/* <Text style={styles.orderText}>Огноо: {order.date}</Text> */}
-      <View style={styles.flex}>
-        <Text style={styles.orderName}>{order.carwashService.name}</Text>
-        <View style={styles.flexz}>
-          <Text style={styles.orderName}>{order.price}₮</Text>
-          <Feather name='chevron-down' style={[styles.icon ]} />
+      <TouchableOpacity onPress={()=>setIsClicked(!isClicked)}>
+
+        <View style={styles.flex}>
+          <Text style={styles.orderName}>{order.carwashService.name}</Text>
+          <View style={styles.flexz}>
+            <Text style={styles.orderName}>{order.price}₮</Text>
+            {
+              isClicked ? (
+                <Feather name='chevron-up' style={[styles.icon ]} />
+              )
+              :(
+                <Feather name='chevron-down' style={[styles.icon ]} />
+              )
+            }
+            
+          </View>
         </View>
-      </View>
-      <View style={[styles.flex, styles.info]}>
-        <Text style={styles.text14}>{order.carwashService.location}</Text>
-       
-          <Text style={styles.text14}>
-            {/* {order.date} */}  2024-08-24
-            </Text>
-       
+        <View style={[styles.flex, styles.info]}>
+          <Text style={styles.text14}>{order.carwashService.location}</Text>
+        
+            <Text style={styles.text14}>
+              {/* {order.date} */}  2024-08-24
+              </Text>
+        </View>
+      </TouchableOpacity>
+      {
+        isClicked && (
+          <View style={[ styles.detailInfo]}>
+        <View style={styles.flex}>
+          <Text style={styles.detailText}>Машины төрөл</Text>
+          <Text style={styles.detailText}>{order.carSize}</Text>
+        </View>
+        <View style={styles.flex}>
+          <Text style={styles.detailText}>Угаалгах төрөл</Text>
+          <Text style={styles.detailText}>{order.washType}</Text>
+        </View>
+        <View style={styles.flex}>
+          <Text style={styles.detailText}>Захиалсан өдөр</Text>
+          <Text style={styles.detailText}>2024-10-11</Text>
+        </View>
+        <View style={styles.flex}>
+          <Text style={styles.detailText}>Захиалсан цаг</Text>
+          <Text style={styles.detailText}>10:00 - 11:00</Text>
+        </View>
+     
       </View>
     
+        )
+      }
+      
     </View>
 </Animated.View>
   );
@@ -68,6 +104,12 @@ const styles = StyleSheet.create({
     // fontWeight: '500',
     color: '#828282',
   },
+  detailText:{
+    fontSize: 14,
+    paddingTop:8,
+    color: '#000',
+    fontWeight:'400'
+  },
   flex: {
     width: '100%',
     flexDirection: 'row',
@@ -81,6 +123,13 @@ const styles = StyleSheet.create({
   },
   info:{
     paddingRight:16
+  },
+  detailInfo:{
+    backgroundColor:"#fff",
+    borderRadius:12,
+    padding:16,
+    paddingTop:8,
+    marginTop:16
   },
   icon: {
     fontSize: 18,
